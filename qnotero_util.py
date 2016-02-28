1f4b70243a7b79d0c7eda2597f6c9064c2d65d85
natto <natto@natto-tpy.none>
2016-02-28 15:06:54 -0500

    upd commentary, detect python3

--- qnotero_util.py
+++ qnotero_util.py
@@ -1,4 +1,6 @@
 '''
+Python3 only
+
 zotero interaction using libzotero by Qnotero
 
 ref https://github.com/smathot/qnotero
@@ -6,13 +8,16 @@ ref http://www.cogsci.nl/blog/tutorials/97-writing-a-command-line-zotero-client-
 (which is an older version using Gnotero, now renamed Qnotero)
 
 '''
-
 import sys
+if sys.version_info[0] < 3:
+    print('Python3 required')
+    sys.exit()
 sys.path.insert(0, '/opt/qnotero')
-from libzotero import libzotero
 
 import os.path as _p
 from glob import glob
+from libzotero import libzotero
+
 ZOTERO_FOLDER_PATH = glob(_p.expanduser('~/.*ozilla/*irefox/Profiles/*.default/zotero'))[0]
 
 zotero = libzotero.LibZotero(ZOTERO_FOLDER_PATH)
@@ -27,15 +32,19 @@ zotero = libzotero.LibZotero(ZOTERO_FOLDER_PATH)
 # item_list = zotero.search('publication:PLOS')
 # combine them with
 # item_list = zotero.search('publication:Science tag:memory')
+#
+# another example:
+# res = zotero.search('fMRI')
+# for i, item in enumerate(res):
+#     print('-'*40)
+#     print('{} of {}'.format(i, len(res)))
+#     print(item.simple_format())
+#     # interesting attributes, e.g.
+#     # item.id
+#     # item.title
+#     # item.tags # list of <str>
+#     # item.fulltext # path to local file
+
 
 if __name__ == '__main__':
-    res = zotero.search('fMRI')
-    for i, item in enumerate(res):
-        print('-'*40)
-        print('{} of {}'.format(i, len(res)))
-        print(item.simple_format())
-        # interesting attributes, e.g.
-        # item.id
-        # item.title
-        # item.tags # list of <str>
-        # item.fulltext # path to local file
+
